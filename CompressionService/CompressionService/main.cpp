c4957218cba9b7f3fe7d21cadd437667edbb1009
killedmyself <markmisra@gmail.com>
2019-12-14 22:37:03 +0200

    Remove redundant comments, remove redundant variables, change function names

--- CompressionService/CompressionService/main.cpp
+++ CompressionService/CompressionService/main.cpp
@@ -1,61 +1,60 @@
 #include <iostream>
 #include <sstream>
 
-std::string parse(std::stringstream& stream) {
-	char character, temp_character, prev_temp_char;  // characters for reading from stream
-	std::ostringstream ret;     // return stream (will be returend as string)
+std::string compress(std::stringstream& stream) {
+	char character = ' ';
+	char next_character = ' ';
+	std::ostringstream compression_result;
 
-	stream >> character;    // get first character
-	int character_count = 1;  // set character_count to 1
-
-	//peek() gets the next character in the stream WITHOUT extracting it!
-	//It is therefore perfect to get a look at upcoming characters without
-	//using them 
-	while ((temp_character = stream.peek()) != EOF) {
-		if (temp_character == character) {  // compare the peeked character with ours
+	stream >> character;    
+	int character_count = 1;  
+	
+	while ((next_character = stream.peek()) != EOF) {
+		if (next_character == character) {  
 			character_count++;
-			prev_temp_char = temp_character;
-			stream >> temp_character;   // this is the real extraction
+			stream >> next_character;   
 		}
 		else {
-			 //if the peeked character is not ours anymore, we push everything
-			 //into our ret stream and reset character_count. I assumed that you
-			 //don't want to print a1 if there is just 1 a so i checked the cases 
+			// replacing a with 1a increases the character count instead of reducing it; 
+			// don't do it
 			if (character_count == 1) {
-				ret << character;
+				compression_result << character;
 			}
+			// replacing aa with 2a doesn't reduce the character count;
+			// don't do it
 			else if (character_count == 2) {
-				ret << character << prev_temp_char;
+				compression_result << character << character;
 			}
 			else {
-				ret << character_count << character;
+				compression_result << character_count << character;
 			}
+
 			character_count = 1;
-			stream >> temp_character;
-			character = temp_character;
+			stream >> next_character;
+			character = next_character;
 		}
 	}
 
-	//Now one more character (and its count) is not pushed into ret, so we have to do this.
+	//Now one more character (and its count) is not pushed into compression_result, so we have to do this.
 	//This is because we peeked and found EOF therefore we stopped 
 	if (character_count == 1) {
-		ret << character;
+		compression_result << character;
 	}
 	else if (character_count == 2) {
-		ret << character << prev_temp_char;
+		compression_result << character << character;
 	}
 	else {
-		ret << character_count << character;
+		compression_result << character_count << character;
 	}
 
-	return ret.str();   // return the stringstream as a string
+	return compression_result.str();   // return the stringstream as a string
 }
 
 int main() {
 	std::string input;
-	getline(std::cin, input);   // get line from std::cin into std::string input
-	std::stringstream ss(input);   // build str::stringstream from input
-	std::string str = parse(ss);    // our function
+	getline(std::cin, input);
+	std::stringstream ss(input);   
+	std::string str = compress(ss);
 	std::cout << str;
 
 	system("pause");
