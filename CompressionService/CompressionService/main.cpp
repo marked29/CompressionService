588df64e37a4743516116276f84db14aa8c62b28
killedmyself <markmisra@gmail.com>
2019-12-15 23:13:58 +0200

    Add class to encapsulate compression logic

--- CompressionService/CompressionService/main.cpp
+++ CompressionService/CompressionService/main.cpp
@@ -1,58 +1,12 @@
 #include <iostream>
-#include <sstream>
-
-void construct_response(std::ostream& result, char symbol, int character_counter) 
-{
-	// replacing a with 1a increases the character count instead of reducing it
-	// don't do it
-	if (character_counter == 1) {
-		result << symbol;
-	}
-	// replacing aa with 2a doesn't reduce the character count;
-	// don't do it
-	else if (character_counter == 2) {
-		result << symbol << symbol;
-	}
-	else {
-		result << character_counter << symbol;
-	}
-}
-
-std::string compress(std::stringstream& stream) {
-	char character = ' ';
-	char next_character = ' ';
-	std::ostringstream compression_result;
-
-	stream >> character;    
-	int character_count = 1;  
-	
-	while ((next_character = stream.peek()) != EOF) {
-		if (next_character == character) {  
-			character_count++;
-			stream >> next_character;   
-		}
-		else {
-			construct_response(compression_result, character, character_count);
-			character_count = 1;
-			stream >> next_character;
-			character = next_character;
-		}
-	}
-
-	// now one more character (and its count) is not pushed into compression_result, so we have to do this.
-	// this is because we peeked and found EOF therefore we stopped 
-	construct_response(compression_result, character, character_count);
-
-	return compression_result.str();
-}
+#include "StringCompressor.h"
 
 int main() {
 	std::string input;
-	getline(std::cin, input);
-	std::stringstream ss(input);   
-	std::string str = compress(ss);
-	std::cout << str;
-
+	std::cin >> input;
+	std::unique_ptr<iDataCompressor> Compressor = std::make_unique<StringCompressor>(input);
+	Compressor->compress_data();
+	//Compressor->get_result();
 	system("pause");
 	return 0;
 }
